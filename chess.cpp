8c546cfc35af2dc0251f20a529c89c5368c2a4b3
JohnFromSpace <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-07 04:01:20 +0200

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,18 +1,3 @@
-//Chess game
-
-public class Spot {
-	private Piece;
-	private int x;
-	private int y;
-};
-
-public Spot(int x, int y, Piece piece)
-{
-	this.setPiece(piece);
-	this.setX(x);
-	this.setY(y);
-}
-
 
 
 
