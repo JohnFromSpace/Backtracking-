3f7973bcd98397e552b071f90e3d3e031a5ca9f2
Boril <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-16 02:07:41 +0200

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,3 +1,27 @@
+//N Queen Problem 
+//Backtracking
+
+#include <stdio.h>
+#include <stdbool.h>
+#define N 4
+
+int ld[30] = { 0 };
+int rd[30] = { 0 };
+int cl[30] = { 0 };
+
+void printSolution(int board[N][N])
+{
+  for(int i = 0; i < N; i++)
+  {
+    for(int j = 0; j < N; j++)
+    {
+      printf(" %d ", board[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+
 
 
 
