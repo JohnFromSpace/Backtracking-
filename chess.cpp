dcc821ca059bc2e06f3a97e589f445f16140d40b
Boril <96446768+JohnFromSpace@users.noreply.github.com>
2022-01-16 15:31:03 +0200

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -5,9 +5,9 @@
 #include <stdbool.h>
 #define N 4
 
-int ld[30] = { 0 };
-int rd[30] = { 0 };
-int cl[30] = { 0 };
+int ld[30] = { 0 }; //left diagonal 
+int rd[30] = { 0 }; //right diagonal
+int cl[30] = { 0 }; 
 
 void printSolution(int board[N][N])
 {
@@ -21,7 +21,21 @@ void printSolution(int board[N][N])
   }
 }
 
-
+bool solveNQueenProblem(int board[N][N], int col)
+{
+  if(col >= N)
+    return true;
+  
+  for(int i = 0; i < N; i++)
+  {
+    /*Check if the queen can be placed on board[i][col]*/
+    /*A check if a queen can be placed on board[row][col]. We just need to check ld[row - col + n - 1] and rd[row + col]*/
+    if( (ld[i - col + N - 1] != 1 && rd[i + col] != 1) && (cl[i] != 1) )
+    {
+         
+    }
+  }
+}
 
 
 
