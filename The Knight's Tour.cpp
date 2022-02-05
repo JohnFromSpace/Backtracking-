fd889bb1c205d82fabfe1cc539289e651765e509
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-05 22:30:24 +0200

    Update The Knight's Tour.cpp

--- The Knight's Tour.cpp
+++ The Knight's Tour.cpp
@@ -11,6 +11,17 @@ int isSafe(int x, int y, int sol[N][N]){
   return (x >= 0 && x< N && y >= 0 && y < N && sol[x][y] == -1);
 }
 
+//Utility function to print the solution matrix
+void printSolution(int sol[N][N]){
+  for(int x = 0; x < N; x++){
+    for(int y = 0; y < N; y++)
+        cout << " " << setw(2) << sol[x][y] << " ";
+      cout << endl;
+  
+  }
+  
+}
+
 
 
 
