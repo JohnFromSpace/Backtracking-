abc7984719910703eba937d8d421fd1421ade9f6
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-06 01:49:40 +0200

    Create Rat in a Maze.cpp

---
+++ Rat in a Maze.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define N 8
+
+
+bool solveMaze(int maze[N][N], int int x, int y, int sol[N][N]);
+
+
+//Function to print out the solution matrix
+void printSolution(int sol[N][N]){
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++)
+      printf(" %d", sol[i][j]);
+    printf("\n");
+  }
+  
+  
+  
+    
+ 
+    
+  
+}
+
+
+
+
+
+
