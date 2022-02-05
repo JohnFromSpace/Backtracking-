e9471cf35d2db02b92901c9f4d9f3e1bc4fddcf9
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-05 22:27:56 +0200

    Update The Knight's Tour.cpp

--- The Knight's Tour.cpp
+++ The Knight's Tour.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N 8
+
+int solveKTUtil(int x, int y, int moveI, int sol[N][N], int xMove[], int yMove[]);
+
+//A utility function to check if i,j are valid indexes for NxN chessboard
+int isSafe(int x, int y, int sol[N][N]){
+  
+  return (x >= 0 && x< N && y >= 0 && y < N && sol[x][y] == -1);
+}
+
+
+
+
+
 
