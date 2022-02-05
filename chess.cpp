16ebc85569323874ca7414a2de72ff0f849467a1
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-05 21:27:36 +0200

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -32,10 +32,26 @@ bool solveNQueenProblem(int board[N][N], int col)
     /*A check if a queen can be placed on board[row][col]. We just need to check ld[row - col + n - 1] and rd[row + col]*/
     if( (ld[i - col + N - 1] != 1 && rd[i + col] != 1) && (cl[i] != 1) )
     {
-         
+      board[i][col] = 1;
+      ld[i - col + N - 1] = rd[i+col]=cl[i] = 1;
+      
+      //recur to place the rest of the queens
+      if(solveNQUtil(board, col + 1))
+        return true;
+      
+      //if placing queen in board[i][col] doesn't lead to a solution, then remove queen from board[i][col]
+      board[i][col] = 0;//Backtracking
+      ld[i - col + N - 1] = rd[i + col] = cl[i] = 0;      
+  
     }
   }
+  //if the queen cannot be placed in any row in this column
+  return false;
 }
+
+
+
+
 
 
 
