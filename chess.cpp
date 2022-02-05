cbb2949421e750e92069bd69e4db60006d24e608
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-05 21:34:18 +0200

    Finished

--- chess.cpp
+++ chess.cpp
@@ -9,29 +9,23 @@ int ld[30] = { 0 }; //left diagonal
 int rd[30] = { 0 }; //right diagonal
 int cl[30] = { 0 }; 
 
-void printSolution(int board[N][N])
-{
-  for(int i = 0; i < N; i++)
-  {
-    for(int j = 0; j < N; j++)
-    {
+void printSolution(int board[N][N]){
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < N; j++){
       printf(" %d ", board[i][j]);
     }
     printf("\n");
   }
 }
 
-bool solveNQueenProblem(int board[N][N], int col)
-{
+bool solveNQueenProblem(int board[N][N], int col){
   if(col >= N)
     return true;
   
-  for(int i = 0; i < N; i++)
-  {
+  for(int i = 0; i < N; i++){
     /*Check if the queen can be placed on board[i][col]*/
     /*A check if a queen can be placed on board[row][col]. We just need to check ld[row - col + n - 1] and rd[row + col]*/
-    if( (ld[i - col + N - 1] != 1 && rd[i + col] != 1) && (cl[i] != 1) )
-    {
+    if( (ld[i - col + N - 1] != 1 && rd[i + col] != 1) && (cl[i] != 1) ){
       board[i][col] = 1;
       ld[i - col + N - 1] = rd[i+col]=cl[i] = 1;
       
@@ -49,7 +43,28 @@ bool solveNQueenProblem(int board[N][N], int col)
   return false;
 }
 
+bool solveNQ(){
+  int board[N][N] = {{0, 0, 0, 0}, 
+                     {0, 0, 0, 0},
+                     {0, 0, 0, 0},
+                     {0, 0, 0, 0}};
+  if(solveNQUtil(board, 0) == false){
+    printf("Solution does not exist.");
+    return false;
+  }
+  
+  
+  printSolution(board);
+  return true;
+  
+                    
+}
 
+
+int main(){ 
+  solveNQ();
+  return 0;
+}
 
 
 
