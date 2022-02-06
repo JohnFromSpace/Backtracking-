3df013dee32210c59a015256eebb46b31986ee35
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-07 01:43:45 +0200

    Update Subset Sum.cpp

--- Subset Sum.cpp
+++ Subset Sum.cpp
@@ -28,6 +28,49 @@ int comparator(const void *pLhs, const void *pRhs){
 // ite          - nodes count
 // target_sum   - sum to be found
 
+void subset_sum(int s[], int t[], int size_t, int s_size, int t_size, int sum, int ite, int const target_sum){
+  total_nodes++;
+  
+  if(target_sum == sum){
+    printSubset(t, t_size);
+    
+    if(ite + 1 < s_size && sum - s[ite] + s[ite + 1] <= target_sum){
+     
+      //Exculde previous added item and consider next candidate
+      subset_sum(s, t, s_size, t_size - 1, sum - s[ite], ite + 1, target_sum);
+      
+    }
+     return; 
+  }
+  else{
+    
+    //Constraint check
+    if(ite < s_size && sum + s[ite] <= target_sum){
+      
+      //Generate nodes along the breadth 
+      for(int i = ite; i < size; i++){
+        t[t_size] = s[i];
+        if(sum + s[i] <= target_sum){
+                  
+          //Consider next level node along depth
+          subset_sum(s, t, s_size, t_size + 1, sum + s[i], i + 1, target_sum);
+          
+        }
+      
+      }
+    
+    }
+    
+  }
+  
+}
+
+//Wrapper that prints subsets that sum to target_sum
+void generateSubsets(int s[], int size, int target_sum){
+
+
+}
+
 
 
 
