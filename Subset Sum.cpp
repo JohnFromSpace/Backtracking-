330d92d89f1f45ecf60e0e927e4f44eb9113b9c9
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-07 01:02:25 +0200

    Create Subset Sum.cpp

---
+++ Subset Sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ARRAYSIZE(a) (sizeof(a)/sizeof(a[0]))
+static int total_nodes
+
+//prints subset found
+void printSubset(int A[], int size){
+  for(int i = 0; i < size; i++){
+    cout << " " << A[i]; 
+  }
+  cout << "\n";
+}
+
+
+
