e02d4c3bc057dc4536bd8f7a9276e33aa7823b26
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-12 23:38:07 +0200

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -5,6 +5,26 @@
 #If the assignment doesn’t lead to a solution, then try the next number for the current empty cell. 
 #And if none of the number (1 to 9) leads to a solution, return false and print no solution exists.
 
+#Print grid
+def printGrid(arr):
+  for i in range(9):
+    for j in range(9):
+      print arr[i][j]
+    print ('n')
+    
+def findEmptyLocation(arr, 1):
+  for row in range(9):
+    for col in range(9):
+      if(arr[row][col] == 0):
+        l[0] = row
+        l[1] = col
+        return True
+  return False    
+      
+  
+    
+    
+
 
 
 
