100ae802cb81362a8029cb6fe8de4375874c9aa5
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-13 23:47:06 +0200

    Update Hamiltonian Cycle.php

--- Hamiltonian Cycle.php
+++ Hamiltonian Cycle.php
@@ -29,7 +29,7 @@ function isSafe($v, $graph, &$path, $pos)
 
 /* A recursive utility function
 to solve hamiltonian cycle problem */
-function hamiltonianCycleSolution($graph, &$path, $pos)
+function hamiltonianCycleUtility($graph, &$path, $pos)
 {
   global $V;
   
@@ -48,7 +48,29 @@ function hamiltonianCycleSolution($graph, &$path, $pos)
   // Try different vertices as a next candidate in
   // Hamiltonian Cycle. We don't try for 0 as we
   // included 0 as starting point hamCycle()
+  for($v = 1; $v < $V; $v++)
+  {
+    /* Check if this vertex can be added
+        to Hamiltonian Cycle */
+    
+    if(isSafe($v, $graph, $path, $pos))
+    {
+      $path[$pos] = $v;
+      
+      //Recur the construct to the rest of the path
+      if(hamiltonianCycleUtility($graph, $path, $pos + 1, ) == true)
+        return true;
+      
+      //If a vertex does not lead to a solution, remove it
+      $path[$pos] = -1;
+    
+    }
   
+  }
+
+  /* If no vertex can be added to Hamiltonian Cycle
+    constructed so far, then return false */
+  return false;
   
 }
 
