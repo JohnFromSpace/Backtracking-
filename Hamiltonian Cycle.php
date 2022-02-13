663bf9720acf4f4c59320e44b1e0daeec2b5f5bf
Boril Ignatov <96446768+JohnFromSpace@users.noreply.github.com>
2022-02-13 23:53:41 +0200

    Update Hamiltonian Cycle.php

--- Hamiltonian Cycle.php
+++ Hamiltonian Cycle.php
@@ -74,6 +74,22 @@ function hamiltonianCycleUtility($graph, &$path, $pos)
   
 }
 
+/* This function solves the Hamiltonian Cycle problem using
+Backtracking. It mainly uses hamCycleUtil() to solve the
+problem. It returns false if there is no Hamiltonian Cycle
+possible, otherwise return true and prints the path.
+Please note that there may be more than one solutions,
+this function prints one of the feasible solutions. */
+function hamCycle($graph)
+{
+  global $V;
+  $path = array_fill(0, $V, 0);
+  for($i = 0; $i < $V; $i++)
+    $path[$i] = -1;
+  
+  
+
+}
 
 
 
